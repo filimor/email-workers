7819e988d3eaa3941001bff25aa151caf9812c83
Filipe Moreira <filimor@posteo.com.br>
2020-08-24 16:05:28 -0300

    Use multiple worker containers

--- worker/worker.py
+++ worker/worker.py
@@ -5,6 +5,7 @@ from random import randint
 
 if __name__ == '__main__':
   r = redis.Redis(host='queue', port=6379, db=0)
+  print('Waiting for messages...')
   while True:
     message = json.loads(r.blpop('sender')[1])
     # Simulating e-mail sending...
