8a0be9aefddb107489149d0a407012b1b30c01db
Filipe Moreira <filimor@posteo.com.br>
2020-08-24 15:58:26 -0300

    Create workers

--- app/sender.py
+++ app/sender.py
@@ -1,28 +1,38 @@
 import psycopg2
-from bottle import route, run, request
+import redis
+import json
+from bottle import Bootle, request
 
-DSN = 'dbname=email_sender user=postgres password=postgres host=db'
-SQL = 'INSERT INTO emails (subject, message) VALUES (%s, %s)'
 
-def register_message(subject, message):
-  conn = psycopg2.connect(DSN)
-  cur = conn.cursor()
-  cur.execute(SQL, (subject, message))
-  conn.commit()
-  cur.close()
-  conn.close()
+class Sender(Bottle):
+  def __init__(self):
+    super().__init__()
+    self.route('/', method='POST', callback=self.send)
+    self.queue = redis.StrictRedis(host='queue', port=6379, db=0)
+    DSN = 'dbname=email_sender user=postgres password=postgres host=db'
+    self.conn = psycopg2.connect(DSN)
 
-  print('Registered message !')
+  def register_message(self, subject, message):
+    SQL = 'INSERT INTO emails (subject, message) VALUES (%s, %s)'
+    cur = self.conn.cursor()
+    cur.execute(SQL, (subject, message))
+    self.conn.commit()
+    cur.close()
 
-@route('/', method='POST')
-def send():
-  subject = request.forms.get('subject')
-  message = request.forms.get('message')
+    msg = {'subject': subject, 'message': message}
+    self.queue.rpush('sender', json.dumps(msg))
 
-  register_message(subject, message)
-  return 'Queued message! Subject: {} Message: {}'.format(
-    subject, message
-  )
+    print('Registered message !')
+
+  def send(self):
+    subject = request.forms.get('subject')
+    message = request.forms.get('message')
+
+    self.register_message(subject, message)
+    return 'Queued message! Subject: {} Message: {}'.format(
+      subject, message
+    )
 
 if __name__ == '__main__':
-  run(host='0.0.0.0', port=8080, debug=True)
+  sender = Sender()
+  sender.run(host='0.0.0.0', port=8080, debug=True)
---
+++ worker/worker.py
@@ -0,0 +1,13 @@
+import redis
+import json
+from time import sleep
+from random import randint
+
+if __name__ == '__main__':
+  r = redis.Redis(host='queue', port=6379, db=0)
+  while True:
+    message = json.loads(r.blpop('sender')[1])
+    # Simulating e-mail sending...
+    print('Enviando a mensagem:', message['subject'])
+    sleep(randint(15,45))
+    print('Mensagem', message['subject'], 'enviada')
